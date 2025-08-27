c29c80fd5648492d4a50c439ed19e75d82273c24
Alex Crease <119538960+Apollo-XIV@users.noreply.github.com>
2025-08-27 10:45:53 +0100

    Adder program (#1)
    * Made adder.py functional
    
    * Added error handling to adder.py

--- adder.py
+++ adder.py
@@ -1,3 +1,8 @@
 
 def adder():
-    print("no op")
+    try:
+        a = int(input("Enter the first number to add:\n"))
+        b = int(input("Enter the second number to add:\n"))
+        print(f"You got: {a + b}")
+    except:
+        print("Invalid input!")
