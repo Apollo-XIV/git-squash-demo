7f69553c470456f085e8f8e2f8e55a2db9cd1014
Alex Crease <alex@crease.sh>
2025-08-27 10:28:58 +0100

    Added error handling to adder.py

--- adder.py
+++ adder.py
@@ -1,5 +1,8 @@
 
 def adder():
-    a = int(input("Enter the first number to add:\n"))
-    b = int(input("Enter the second number to add:\n"))
-    print(f"You got: {a + b}")
+    try:
+        a = int(input("Enter the first number to add:\n"))
+        b = int(input("Enter the second number to add:\n"))
+        print(f"You got: {a + b}")
+    except:
+        print("Invalid input!")
